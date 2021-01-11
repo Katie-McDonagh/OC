181c23c9cb1514f868a9da9e3ebddc78f2b37bf9
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-01-11 14:41:46 +0000

    card has top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,14 @@
 require 'oystercard'
 
 describe Oystercard do
+
   it "has a balance of 0 upon initialization" do
     expect(subject.balance).to equal(0)
   end
+
+  it "can be topped up" do
+    subject.top_up(10)
+    expect(subject.balance).to equal(10)
+  end
+  
 end
